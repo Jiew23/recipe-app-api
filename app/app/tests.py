45a98e1092c5d3a6803fb94c38addc416e33faf7
Jiew23 <970430296@qq.com>
2025-01-19 15:03:12 +0800

    Add wait_for_db command to fix race condition between app and db

--- app/app/tests.py
+++ app/app/tests.py
@@ -3,21 +3,22 @@ Sample tests
 """
 
 from django.test import SimpleTestCase
-import unittest
+import unittest # noqa
 
 from app import calc
+
 
 class ClacTests(SimpleTestCase):
     """Test the calc module."""
 
     def test_add_numbers(self):
         """Test adding numbers together."""
-        res = calc.add(5,6)
+        res = calc.add(5, 6)
 
         self.assertEqual(res, 11)
-    
+
     def test_subtract_numbers(self):
         """Test subtracting numbers."""
-        res = calc.subtract(10,15)
-        
-        self.assertEqual(res,5)
+        res = calc.subtract(10, 15)
+
+        self.assertEqual(res, 5)
